8f62360b89849259ebc820425fdfe35882e1d505
Полина <polinachetc@gmail.com>
2025-02-18 22:21:32 +0300

    HW 3 fix

--- src/main/java/service/OrderService.java
+++ src/main/java/service/OrderService.java
@@ -1,13 +1,55 @@
 package service;
 
+import config.ConfigLoader;
 import dto.OrderDTO;
+import io.restassured.response.ValidatableResponse;
+
+import static io.restassured.RestAssured.given;
 
 public class OrderService {
-    public OrderDTO createOrderDTO(Long petId, Integer quantity, boolean complete) {
-        return OrderDTO.builder()
-                .petId(petId)
-                .quantity(quantity)
-                .complete(complete)
-                .build();
+    private final String baseUrl;
+    private static final String STORE_ORDER_ORDER_ID = "store/order/{orderId}";
+    private static final String STORE_ORDER = "store/order";
+    public OrderService() {
+        this.baseUrl = new ConfigLoader().getBaseUrl();
+    }
+
+    public ValidatableResponse createOrderFind(OrderDTO order) {
+        return given()
+                .baseUri(baseUrl)
+                .contentType("application/json")
+                .log().all()
+                .basePath(STORE_ORDER_ORDER_ID)
+                .pathParam("orderId", order.getOrderId())
+                .when()
+                .get()
+                .then()
+                .log().all();
+    }
+
+    public ValidatableResponse createOrder(OrderDTO order) {
+        return given()
+                .baseUri(baseUrl)
+                .contentType("application/json")
+                .log().all()
+                .basePath(STORE_ORDER)
+                .body(order)
+                .when()
+                .post()
+                .then()
+                .log().all();
+    }
+
+    public ValidatableResponse deleteOrder(OrderDTO order) {
+        return given()
+                .baseUri(baseUrl)
+                .contentType("application/json")
+                .log().all()
+                .basePath(STORE_ORDER_ORDER_ID)
+                .pathParam("orderId", order.getOrderId())
+                .when()
+                .delete()
+                .then()
+                .log().all();
     }
 }
--- src/test/java/test/OrderTest.java
+++ src/test/java/test/OrderTest.java
@@ -11,44 +11,48 @@ import service.OrderService;
 
 import javax.inject.Inject;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class OrderTest {
     private OrderDTO orderDTO;
     private Long orderId;
-    private static final String PET_STORE_SWAGGER = "https://petstore.swagger.io/v2";
-    private static final String STORE_ORDER_ORDER_ID = "store/order/{orderId}";
-    private static final String STORE_ORDER = "store/order";
+
     @Inject
     private OrderService orderService;
 
+
     @BeforeEach
-    public void beforeTest() {
+    public void setUp() {
         var injector = Guice.createInjector(new OrderModule());
         injector.injectMembers(this);
 
-        orderDTO = orderService.createOrderDTO(1L, 0, true);
+        orderService = new OrderService();
 
-        var response = createOrder(orderDTO);
+        orderDTO = OrderDTO.builder()
+                .petId(1L)
+                .quantity(0)
+                .complete(true)
+                .build();
+
+        var response = orderService.createOrder(orderDTO);
         response.statusCode(200);
         orderId = response.extract().path("id");
     }
 
     @AfterEach
-    public void afterTest() {
+    public void tearDown() {
         if (orderId != null) {
             var orderToDelete = OrderDTO.builder()
                     .orderId(orderId)
                     .build();
 
-            deleteOrder(orderToDelete).statusCode(200);
+            orderService.deleteOrder(orderToDelete).statusCode(200);
         }
     }
 
     @Test
-    public void checkOrderExist() {
-        createOrder(orderDTO)
+    public void checkOrderExists() {
+        orderService.createOrder(orderDTO)
                 .statusCode(200)
                 .body("petId", equalTo(1))
                 .body("quantity", equalTo(0))
@@ -57,13 +61,13 @@ public class OrderTest {
 
     @Test
     public void checkCreateNewOrder() {
-        var order = OrderDTO.builder()
+        var newOrder = OrderDTO.builder()
                 .petId(15L)
                 .quantity(1000)
                 .complete(true)
                 .build();
 
-        createOrder(order)
+        orderService.createOrder(newOrder)
                 .statusCode(200)
                 .body("petId", equalTo(15))
                 .body("quantity", equalTo(1000))
@@ -72,70 +76,37 @@ public class OrderTest {
 
     @Test
     public void checkDeleteOrder() {
-        var order = OrderDTO.builder()
+        var orderToCreate = OrderDTO.builder()
                 .petId(15L)
                 .quantity(1000)
                 .complete(true)
                 .build();
 
-        var response = createOrder(order);
+        var response = orderService.createOrder(orderToCreate);
         response.statusCode(200);
 
-        Long orderId = response.extract().path("id");
-        order.setOrderId(orderId);
+        Long createdOrderId = response.extract().path("id");
+        orderToCreate.setOrderId(createdOrderId);
 
-        deleteOrder(order)
+        orderService.deleteOrder(orderToCreate)
                 .statusCode(200);
     }
 
     @Test
-    public void checkOrderNotExist() {
-        createOrderFind(OrderDTO.builder()
-                .orderId(9999L)
-                .build()
-        )
+    public void checkOrderNotExists() {
+        orderService.createOrderFind(OrderDTO.builder()
+                        .orderId(9999L)
+                        .build())
                 .statusCode(404)
                 .body("code", equalTo(1))
                 .body("type", equalTo("error"))
                 .body("message", equalTo("Order not found"));
     }
-
-    private ValidatableResponse createOrderFind(OrderDTO order) {
-        return given()
-                .baseUri(PET_STORE_SWAGGER)
-                .contentType("application/json")
-                .log().all()
-                .basePath(STORE_ORDER_ORDER_ID)
-                .pathParam("orderId", order.getOrderId())
-                .when()
-                .get()
-                .then()
-                .log().all();
-    }
-
-    private ValidatableResponse createOrder(OrderDTO order) {
-        return given()
-                .baseUri(PET_STORE_SWAGGER)
-                .contentType("application/json")
-                .log().all()
-                .basePath(STORE_ORDER)
-                .body(order)
-                .when()
-                .post()
-                .then()
-                .log().all();
-    }
-
-    private ValidatableResponse deleteOrder(OrderDTO order) {
-        return given()
-                .baseUri(PET_STORE_SWAGGER)
-                .contentType("application/json")
-                .log().all()
-                .basePath(STORE_ORDER_ORDER_ID)
-                .pathParam("orderId", order.getOrderId())
-                .when()
-                .delete()
-                .then()
-                .log().all();
-    }
 }
+
+
+
+
+
+
+
